c5058d2cd06f94641ee83787ead509d17128bacb
Amine Nait Bachir <anaitbachir@alteca.fr>
2024-04-17 11:49:56 +0200

    Add another page

---
+++ WebApplication1/Pages/Test.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class TestModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TestModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
